fbb996a67c504891c541fab4f9b8e185dfa7efcf
Karasawa Takumi <karasawa.takumi@tensorverse.jp>
2024-09-23 18:42:25 +0900

    Update modelData.js

--- docs/modelData.js
+++ docs/modelData.js
@@ -2,12 +2,14 @@ const models = {
     objectDetection: 'https://649vecmu32.execute-api.ap-northeast-1.amazonaws.com/tas',
     segmentation: 'https://7plx8cens8.execute-api.ap-northeast-1.amazonaws.com/tas',
     depthEstimation: 'https://sw97avsdr8.execute-api.ap-northeast-1.amazonaws.com/tas',
-    backgroundRemoval: 'https://ikpjy7mps4.execute-api.ap-northeast-1.amazonaws.com/tas'
+    backgroundRemoval: 'https://ikpjy7mps4.execute-api.ap-northeast-1.amazonaws.com/tas',
+    trimming: 'https://ndcs28dp14.execute-api.ap-northeast-1.amazonaws.com/tas',
 };
 
 const modelDescriptions = {
     objectDetection: "物体検出: 画像内の物体を検出し、対応するバウンディングボックスを出力します。",
     segmentation: "セグメンテーション: 画像の各ピクセルをクラスに分類し、対象領域をマスクとして出力します。",
     depthEstimation: "深度推定: 画像内の各ピクセルの深度を推定し、距離情報を取得します。",
-    backgroundRemoval: "背景除去: 画像から前景と背景を分離し、背景を除去します。"
+    backgroundRemoval: "背景除去: 画像から前景と背景を分離し、背景を除去します。",
+    trimming: "テスト"
 };
