ad548199c5f44d2276477c3386dc84f9fd34b3eb
Karasawa Takumi <karasawa.takumi@tensorverse.jp>
2025-04-08 00:44:41 +0900

    Update schema.rb

--- liam/schema.rb
+++ liam/schema.rb
@@ -1,17 +1,17 @@
 ActiveRecord::Schema.define(version: 20250408000000) do
 
     create_table "Python", force: :cascade do |t|
-      t.string   "Variable",
-      t.string   "Function"
-      t.string   "Class",
-      t.string   "Main Guard"
-      t.string   "開発言語"
+      t.string "Variable"
+      t.string "Function"
+      t.string "Class"
+      t.string "Main_Guard"
     end
   
     create_table "AI", force: :cascade do |t|
-      t.string   "Neural Network",
-      t.string   "Deep Learning",
-      t.string   "機械学習フレームワーク"
+      t.string  "Neural_Network"
+      t.string  "Deep_Learning"
+      t.references :Python, foreign_key: { to_table: :Python }
+      t.string  "機械学習フレームワーク"
     end
   
   end
